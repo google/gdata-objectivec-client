65e99c8597b1a7ae59988a26d0beef055dbdd476
gregrobbins <gregrobbins@f334b912-182a-0410-b59a-f50d4d03ee1a>
2008-03-18 01:42:24 +0000

    Fix some test data

--- trunk/Source/Elements/GDataWho.m
+++ trunk/Source/Elements/GDataWho.m
@@ -37,7 +37,7 @@
 
 @implementation GDataWho
 // a who entry, as in
-// <gd:who rel="http://schemas.google.com/g/2005#event.organizer" valueString="Greg Robbins" email="test@coldnose.net">
+// <gd:who rel="http://schemas.google.com/g/2005#event.organizer" valueString="Fred Flintstone" email="fred@domain.com">
 //   <gd:attendeeStatus value="http://schemas.google.com/g/2005#event.accepted"/>
 // </gd:who>
 //
