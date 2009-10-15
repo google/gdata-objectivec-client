f064f9a9edcef71bc42d5cd6216fd12aa0f3c630
gregrobbins <gregrobbins@f334b912-182a-0410-b59a-f50d4d03ee1a>
2009-10-15 00:04:43 +0000

    Add display attribute to GDataWebContent

--- trunk/Source/Clients/Calendar/GDataWebContent.m
+++ trunk/Source/Clients/Calendar/GDataWebContent.m
@@ -1,17 +1,17 @@
 /* Copyright (c) 2007-2008 Google Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 //
 //  GDataWebContent.m
@@ -23,6 +23,7 @@
 #import "GDataWebContent.h"
 #import "GDataEntryCalendarEvent.h"
 
+static NSString* const kDisplayAttr = @"display";
 static NSString* const kHeightAttr = @"height";
 static NSString* const kWidthAttr = @"width";
 static NSString* const kURLAttr = @"url";
@@ -124,6 +125,14 @@ static NSString* const kURLAttr = @"url";
   [self setStringValue:str forAttribute:kURLAttr];
 }
 
+- (NSString *)display {
+  return [self stringValueForAttribute:kDisplayAttr];
+}
+
+- (void)setDisplay:(NSString *)str {
+  [self setStringValue:str forAttribute:kDisplayAttr];
+}
+
 // extensions
 
 - (NSArray *)gadgetPreferences {
@@ -144,11 +153,8 @@ static NSString* const kURLAttr = @"url";
   // step through all preferences and add their name/values to a dictionary
   NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
   
-  NSArray *prefs = [self gadgetPreferences];
-  NSUInteger numPrefs = [prefs count];
-  
-  for (NSUInteger idx = 0; idx < numPrefs; idx++) {
-    GDataWebContentGadgetPref *pref = [prefs objectAtIndex:idx];
+  GDataWebContentGadgetPref *pref;
+  GDATA_FOREACH(pref, [self gadgetPreferences]) {
     [dictionary setObject:[pref value] forKey:[pref name]];
   }
 
