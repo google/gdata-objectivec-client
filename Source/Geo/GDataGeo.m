aa5beeec7101f2db68c389edbb896e72000b3825
gregrobbins <gregrobbins@f334b912-182a-0410-b59a-f50d4d03ee1a>
2010-04-30 20:33:12 +0000

    Add Geo radius and featurename

--- Source/Geo/GDataGeo.m
+++ Source/Geo/GDataGeo.m
@@ -1,17 +1,17 @@
 /* Copyright (c) 2007 Google Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 //
 //  GDataGeo.m
@@ -45,12 +45,12 @@
                                             namespaceURI:[element URI]
                                              fromElement:element];
     NSNumber *latNum = [self doubleNumberValueFromElement:latElem];
-    
+
     NSXMLElement *longElem = [self childWithQualifiedName:@"geo:long"
                                              namespaceURI:[element URI]
                                               fromElement:element];
     NSNumber *longNum = [self doubleNumberValueFromElement:longElem];
-    
+
     if (latNum && longNum) {
       [self setValues:[NSArray arrayWithObjects:latNum, longNum, nil]];
     }
@@ -60,10 +60,10 @@
 
 
 - (NSXMLElement *)XMLElement {
-  
+
   NSXMLElement *element = [self XMLElementWithExtensionsAndDefaultName:@"geo:Point"];
   NSArray *values = [self values];
-  
+
   if ([self isPoint]) {
     NSNumber *latNum = [values objectAtIndex:0];
     NSNumber *longNum = [values objectAtIndex:1];
@@ -74,16 +74,16 @@
     if (nameFromXML) {
       prefix = [NSXMLNode prefixForName:nameFromXML];
     }
-    
+
     NSString *latName = [NSString stringWithFormat:@"%@:lat", prefix];
     NSString *longName = [NSString stringWithFormat:@"%@:long", prefix];
-    
+
     [self addToElement:element
- childWithStringValueIfNonEmpty:[latNum stringValue]
+childWithStringValueIfNonEmpty:[latNum stringValue]
               withName:latName];
 
     [self addToElement:element
- childWithStringValueIfNonEmpty:[longNum stringValue]
+childWithStringValueIfNonEmpty:[longNum stringValue]
               withName:longName];
   }
   return element;
@@ -105,14 +105,14 @@
   self = [super initWithXMLElement:element
                             parent:parent];
   if (self) {
-    
+
     NSString *valueListStr = [self stringValueFromElement:element];
     if ([valueListStr length] > 0) {
-      
+
       NSArray *values = [GDataGeo valuesWithCoordinateString:valueListStr];
       if ([values count] > 0) {
-        
-        [self setValues:values]; 
+
+        [self setValues:values];
       }
     }
   }
@@ -121,7 +121,7 @@
 
 
 - (NSXMLElement *)XMLElement {
-  
+
   // our superclass defaults to making a georss:point, so we'll call that
   return [super XMLElement];
 }
@@ -158,47 +158,47 @@
                                                   fromElement:element];
     if (gmlPointElem) {
       NSXMLElement *gmlPosElem = [self childWithQualifiedName:@"gml:pos"
-                                                   namespaceURI:kGDataNamespaceGeoGML
-                                                    fromElement:gmlPointElem];
+                                                 namespaceURI:kGDataNamespaceGeoGML
+                                                  fromElement:gmlPointElem];
       if (gmlPointElem) {
-        
+
         NSString *valueListStr = [self stringValueFromElement:gmlPosElem];
         if ([valueListStr length] > 0) {
-          
+
           NSArray *values = [GDataGeo valuesWithCoordinateString:valueListStr];
           if ([values count] > 0) {
-            
-            [self setValues:values]; 
+
+            [self setValues:values];
           }
         }
       }
     }
-    
+
   }
   return self;
 }
 
 
 - (NSXMLElement *)XMLElement {
-  // generate 
+  // generate
   // <georss:where> <gml:Point> <gml:pos>
   //    latitude longitude
   // </gml:pos> </gml:Point> </georss:where>
-  
+
   NSXMLElement *element = [self XMLElementWithExtensionsAndDefaultName:@"georss:where"];
-  
+
   if ([self isPoint]) {
-        
+
     NSString *valueListStr = [self coordinateString];
-    
+
     NSXMLElement *gmlPointElem = [NSXMLElement elementWithName:@"gml:Point"];
-    
+
     [self addToElement:gmlPointElem
- childWithStringValueIfNonEmpty:valueListStr
+childWithStringValueIfNonEmpty:valueListStr
               withName:@"gml:pos"];
-    
+
     [element addChild:gmlPointElem];
-    
+
   }
   return element;
 }
@@ -208,29 +208,29 @@
 @implementation GDataGeo
 
 + (NSDictionary *)geoNamespaces {
-  
+
   NSMutableDictionary *namespaces = [NSMutableDictionary dictionary];
-  
+
   [namespaces setObject:kGDataNamespaceGeoRSS
                  forKey:kGDataNamespaceGeoRSSPrefix]; // "georss"
-  
-  [namespaces setObject:kGDataNamespaceGeoW3C 
+
+  [namespaces setObject:kGDataNamespaceGeoW3C
                  forKey:kGDataNamespaceGeoW3CPrefix]; // "geo"
-  
+
   [namespaces setObject:kGDataNamespaceGeoGML
                  forKey:kGDataNamespaceGeoGMLPrefix]; // "gml"
-  
-  return namespaces;  
+
+  return namespaces;
 }
 
 + (id)geoWithLatitude:(double)latitude
             longitude:(double)longitude {
   GDataGeo* obj = [[[self alloc] init] autorelease];
-  
+
   NSNumber *latNum = [NSNumber numberWithDouble:latitude];
   NSNumber *longNum = [NSNumber numberWithDouble:longitude];
   [obj setValues:[NSArray arrayWithObjects:latNum, longNum, nil]];
-  
+
   return obj;
 }
 
@@ -239,7 +239,7 @@
   self = [super initWithXMLElement:element
                             parent:parent];
   if (self) {
-    
+
     // to initialize from XML, this must be one of the
     // subclasses that parses the XML appropriately,
     // so we'll use a strict equal-to class test
@@ -254,9 +254,9 @@
   // subclass
   //
   // generate a georss:point
-  
+
   NSXMLElement *element = [self XMLElementWithExtensionsAndDefaultName:@"georss:point"];
-  
+
   if ([self isPoint]) {
     NSString *str = [self coordinateString];
     [element addStringValue:str];
@@ -279,17 +279,17 @@
 
   if (self == other) return YES;
   if (![other isKindOfClass:[GDataGeo class]]) return NO;
-  
+
   return [super isEqual:other]
-    && AreEqualOrBothNil([self values], [other values]);
+  && AreEqualOrBothNil([self values], [other values]);
 }
 
 #if !GDATA_SIMPLE_DESCRIPTIONS
 - (NSMutableArray *)itemsForDescription {
   NSMutableArray *items = [NSMutableArray array];
-  
+
   [self addToArray:items objectDescriptionIfNonNil:values_ withName:@"values"];
-  
+
   return items;
 }
 #endif
@@ -299,7 +299,7 @@
 //
 
 - (NSArray *)values {
-  return values_; 
+  return values_;
 }
 
 - (void)setValues:(NSArray *)array {
@@ -309,7 +309,7 @@
 
 // convenience accessors
 - (BOOL)isPoint {
-  return ([values_ count] == 2); 
+  return ([values_ count] == 2);
 }
 
 - (NSString *)coordinateString {
@@ -320,14 +320,14 @@
 
 - (double)latitude {
   if ([self isPoint]) {
-    return [[values_ objectAtIndex:0] doubleValue]; 
+    return [[values_ objectAtIndex:0] doubleValue];
   }
   return NAN;
 }
 
 - (double)longitude {
   if ([self isPoint]) {
-    return [[values_ objectAtIndex:1] doubleValue]; 
+    return [[values_ objectAtIndex:1] doubleValue];
   }
   return NAN;
 }
@@ -339,17 +339,17 @@
 // valuesWithCoordinateString returns an array of doubles
 // scanned from |str|.  The values must be in pairs
 + (NSArray *)valuesWithCoordinateString:(NSString *)str {
-  
+
   NSMutableArray *array = [NSMutableArray array];
   NSScanner *scanner = [NSScanner scannerWithString:str];
-  
+
   // scan pairs of coordinates
   double val1, val2;
   while ([scanner scanDouble:&val1] && [scanner scanDouble:&val2]) {
-    [array addObject:[NSNumber numberWithDouble:val1]]; 
-    [array addObject:[NSNumber numberWithDouble:val2]]; 
+    [array addObject:[NSNumber numberWithDouble:val1]];
+    [array addObject:[NSNumber numberWithDouble:val2]];
   }
-  
+
   return array;
 }
 
@@ -362,22 +362,22 @@
 // call this in addExtensionDeclarations
 + (void)addGeoExtensionDeclarationsToObject:(GDataObject *)object
                              forParentClass:(Class)parentClass {
-  
+
   // we declare three different extensions which can be geoLocation
-  
+
   [object addExtensionDeclarationForParentClass:parentClass
                                      childClass:[GDataGeoRSSPoint class]];
   [object addExtensionDeclarationForParentClass:parentClass
                                      childClass:[GDataGeoRSSWhere class]];
   [object addExtensionDeclarationForParentClass:parentClass
-                                     childClass:[GDataGeoW3CPoint class]];  
+                                     childClass:[GDataGeoW3CPoint class]];
 }
 
 // utility for the getter for GDataGeo
 + (GDataGeo *)geoLocationForObject:(GDataObject *)object {
-  
+
   // the location point may be any of GDataGeo's three subclass types
-  
+
   GDataGeo *geo = [object objectForExtensionClass:[GDataGeoRSSPoint class]];
   if (!geo) {
     geo = [object objectForExtensionClass:[GDataGeoRSSWhere class]];
@@ -385,26 +385,40 @@
   if (!geo) {
     geo = [object objectForExtensionClass:[GDataGeoW3CPoint class]];
   }
-  return geo;  
+  return geo;
 }
 
 // utility for the setter for GDataGeo
 + (void)setGeoLocation:(GDataGeo *)geo forObject:(GDataObject *)object {
-  
+
   // remove the previous geo, whatever class it was
   GDataGeo *oldGeo = [GDataGeo geoLocationForObject:object];
   if (oldGeo) {
     [object removeObject:oldGeo forExtensionClass:[oldGeo class]];
   }
-  
+
   // GDataGeo itself lacks support for the GDataExtension protocol;
   // only instances of its subclasses can be used as extensions
-  GDATA_ASSERT(![geo isMemberOfClass:[GDataGeo class]], 
+  GDATA_ASSERT(![geo isMemberOfClass:[GDataGeo class]],
                @"setGeoLocation requires an instance of a subclass of GDataGeo");
-  
+
   [object setObject:geo forExtensionClass:[geo class]];
 }
 
+@end
+
+#pragma mark -
+
+@implementation GDataGeoRSSFeatureName
++ (NSString *)extensionElementURI       { return kGDataNamespaceGeoRSS; }
++ (NSString *)extensionElementPrefix    { return kGDataNamespaceGeoRSSPrefix; }
++ (NSString *)extensionElementLocalName { return @"featurename"; }
+@end
+
+@implementation GDataGeoRSSRadius
++ (NSString *)extensionElementURI       { return kGDataNamespaceGeoRSS; }
++ (NSString *)extensionElementPrefix    { return kGDataNamespaceGeoRSSPrefix; }
++ (NSString *)extensionElementLocalName { return @"radius"; }
 @end
 
 #endif // !GDATA_REQUIRE_SERVICE_INCLUDES || GDATA_INCLUDE_*_SERVICE
